1c0a09e54923f1746249b312a82711821df15ec2
Abhisehkkiar15 <mpsystem05@gmail.com>
2025-08-26 09:45:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ from Crypto.Util.Padding import unpad
 from base64 import b64encode, b64decode
 from logs import logging
 from bs4 import BeautifulSoup
-import saini as helper
+import ALLClassesMorena as helper
 from utils import progress_bar
 from vars import API_ID, API_HASH, BOT_TOKEN, OWNER, CREDIT, AUTH_USERS, TOTAL_USERS
 from aiohttp import ClientSession
